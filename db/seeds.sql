256c0109a82ec7bdeafa3d1fc3df775873d0cda0
Juan in a million <59286923+damianjuan@users.noreply.github.com>
2020-07-04 01:20:38 -0500

    initial file and directory set up

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("The Fuh-gouda-bout-it Burger");
+INSERT INTO burgers (burger_name) VALUES ("The Final Kraut Down Burger");
+INSERT INTO burgers (burger_name) VALUES ("I've Created a Munster Burger");
